a53ec2a693516da56d4f1d86fa729ca8c7e22aba
Yugo Sakamoto <yugoccp@gmail.com>
2021-03-20 10:48:25 -0300

    Add functions

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,9 @@ import Header from '@components/Header'
 import Footer from '@components/Footer'
 
 export default function Home() {
+
+  fetch("/.netlify/functions/hello").then(console.log)
+
   return (
     <div className="container">
       <Head>
